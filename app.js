2c91611feb9fcc6a5cfef9cbc2c4fb3c7826f7a8
Vivek Mandal <vivek.mandal@shipyaari.com>
2023-06-12 01:22:11 +0530

    db connection

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const app = express();
 const routes = require('./routes/routes');
 // dotenv.config();
 const PORT = process.env.PORT;
+
+// DB connection
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+.then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+  console.log(`DB Connection Error: ${err.message}`);
+});
 
 // app.use(morgan("dev"));
 app.use('/', routes);
